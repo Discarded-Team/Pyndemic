904e2e65d0a62189c9e568fb0764195d2db21883
LGP <Joesalmon1985@gmail.com>
2016-08-07 18:59:55 +0100

    removed extra print lines from t_makeboard

--- t_makeboard.py
+++ t_makeboard.py
@@ -10,7 +10,6 @@ class T( unittest.TestCase ):
 
 # This def should test if the createcountry
 	def test_setup (self):
-		print "checking table created"
 		sg = startinggame ()
 		sg.setup ( )
 		with sqlite3.connect('pandemic.db') as conn:
@@ -21,7 +20,6 @@ class T( unittest.TestCase ):
 		self.assertEqual(answer,None,'The table for countries was not created.')
 
 	def test_setupboard (self):
-		print "checking table is being populated from testboard.txt"
 		sg = startinggame ()
 		sg.setupboard ('testboard.txt')
 		with sqlite3.connect('pandemic.db') as conn:
@@ -32,10 +30,6 @@ class T( unittest.TestCase ):
 			col1 = answer [0]
 			col2 = answer [1]
 			col4 = answer [3]
-		print "This is the answer", answer
-		print "This is col1", col1
-		print "This is col2", col2
-		print "This is col4", col4
 		self.assertEqual(col1,'Atlanta','The country name was not added to the database')
 		self.assertEqual(col2,2,'The number of countries was not added to the database')
 		self.assertEqual(col4,'Newyork','The names of connecting countries were not added to the database')
