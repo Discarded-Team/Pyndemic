9c692a933fc0135cd891e638bd56f8414eacbb11
Joe <joesalmon1985@gmail.com>
2016-08-07 09:56:14 +0100

    Fixed test to check done working in SQLite.

--- t_makeboard.py
+++ t_makeboard.py
@@ -2,16 +2,23 @@
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 smarttab expandtab:
 
 import unittest
+import sqlite3
 from makeboard import startinggame
 
-global nocountries
 
 class T( unittest.TestCase ):
 
 # This def should test if the createcountry
 	def test_setup (self):
-		nocountries = 'thewrongnumber'
 		sg = startinggame ()
-		sg.setup (nocountries)
-		self.assertEqual(nocountries,0, msg = 'number of countries is not correct')
+		sg.setup ( )
+		with sqlite3.connect('pandemic.db') as conn:
+			cursor = conn.cursor()
+			tobedone = 'SELECT * FROM countries:'
+			cursor.execute( tobedone)
+			answer = cursor.fetchone ( )
+			print answer
+		foundtable = answer [0]
+		print foundtable
+		self.assertEqual(foundtable,0, msg = 'number of countries is not correct')
 
