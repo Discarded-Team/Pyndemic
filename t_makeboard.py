1ebfa02087bff1e06a4bcd0b391864f4ed2cf68c
Joe <joesalmon1985@gmail.com>
2016-08-24 22:41:52 +0100

    unneeded test removed (found in t_inturn.py)

--- t_makeboard.py
+++ t_makeboard.py
@@ -298,35 +298,6 @@ class T( unittest.TestCase ):
 
 
 
-# This def tests the infect cities def.
-        def test_inaturn_infectcities (self):
-                it = inaturn ()
-                sg = startinggame ()
-                sg.BoardTBL ('testboard.txt')
-                sg.idTBL()
-		sg.iddTBL ()
-                sg.shufid ( )
-                it.infectcities (2)
-                with sqlite3.connect('pandemic.db') as conn:
-                        cursor = conn.cursor()
-                        tobedone = """SELECT * FROM boardTBL WHERE rcube >= 1 or bcube >= 1 or ycube >= 1 or pcube >= 1 or ucube >= 1; """
-                        cursor.execute( tobedone)
-                        answerX = cursor.fetchall ( )
-                        tobedone = """SELECT * FROM iddTBL; """
-                        cursor.execute( tobedone)
-                        answerY = cursor.fetchone ( )
-                        answer2 = answerY [0]
-                        tobedone = """SELECT rcube,bcube,ycube,ucube,pcube FROM boardTBL WHERE name is '%s';""" % (answer2)
-                        cursor.execute( tobedone)
-                        answerZ = cursor.fetchone ( )
-			answer4a = answerZ [0]
-			answer4b = answerZ [1]
-			answer4c = answerZ [2]
-			answer4d = answerZ [3]
-			answer4e = answerZ [4]
-			answer4 = answer4a + answer4b + answer4c + answer4d + answer4e
-                self.assertNotEqual(answerX,None,'Something wrong')
-                self.assertEqual(answer4,1,'A city card in the infection discard pile has no infection cubes on')
 
 
 # This checks the infection deck has been set up
