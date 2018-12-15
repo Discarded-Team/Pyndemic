aa462e900e1f626f20c5c006eae324ba3a93dda4
joe <Joesalmon1985@gmail.com>
2018-12-15 11:45:49 +0000

    slowly putting together thing

--- Pandemic/PandemicGame.java
+++ Pandemic/PandemicGame.java
@@ -1,8 +1,15 @@
 public class PandemicGame 
-	{
+{
+	public String gameName;
+
 	public static void main(String[] args) 
-		{
+	{
 		System.out.println("this is the game!");
-		}	
+	}	
+
+	public PandemicGame ()
+	{
+		gameName = "the anme";
 	}
+}
 
