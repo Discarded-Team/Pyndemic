0037d8a9e3721b49a0d92f470e55bd0ab22e3e52
eugenyProchan <pro4anjey@gmail.com>
2020-05-26 22:43:38 +0500

    update setup.py
    add settings.cfg to package_data field
    wrote name pyndemic and pyndemic.ui instead find_packages()

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Pyndemic",
-    version="0.1.0",
-    packages=find_packages('src', exclude=['test']),
-    author="Discarded Team",
-    author_email="geroyparagvay@gmail.com",
-    description="A future digital version of Pandemic board game",
+    name='Pyndemic',
+    version='0.1.0',
+    author='Discarded Team',
+    author_email='geroyparagvay@gmail.com',
+    description='A future digital version of Pandemic board game',
     license='GNU',
+    packages=['pyndemic', 'pyndemic.ui'],
+    include_package_data=True,
+    package_data={'pyndemic': ['settings.cfg']}
 )
