80c317c9f140855da53f0ac47583d9a03bef2b04
D-Mantrid <geroyparagvay@gmail.com>
2020-06-07 16:31:16 +0300

    Updated tests for `core.game_entity` and `core.api` module.
    Provided test case for `GameEntityCreationMeta` and simplified test case for `GameEntity`.
    Refined codestyle for APITestCase.

--- test/test_core/test_api.py
+++ test/test_core/test_api.py
@@ -1,31 +1,30 @@
 import unittest
-from pyndemic.core.api import RequestTypes, ResponseTypes, \
-    GameplayCommands, termination_request, empty_response, \
-    final_response, message_response
+from pyndemic.core.api import (RequestTypes, ResponseTypes, GameplayCommands,
+                               termination_request, empty_response,
+                               final_response, message_response)
 
 
 class APITestCase(unittest.TestCase):
     def test_termination_request(self):
         response = termination_request()
-        required = RequestTypes.TERMINATION.value
-        self.assertEqual(response['type'], required)
+        required_type = RequestTypes.TERMINATION
+        self.assertEqual(response['type'], required_type)
 
     def test_empty_response(self):
         response = empty_response()
-        required = ResponseTypes.EMPTY.value
-        self.assertEqual(response['type'], required)
+        required_type = ResponseTypes.EMPTY
+        self.assertEqual(response['type'], required_type)
 
     def test_final_response(self):
         message = "see ya"
         response = final_response(message)
-        required = ResponseTypes.TERMINATION.value
-        self.assertEqual(response['type'], required)
+        required_type = ResponseTypes.TERMINATION
+        self.assertEqual(response['type'], required_type)
         self.assertEqual(response['message'], message)
 
     def test_message_response(self):
         message = "something to say"
         response = message_response(message)
-        required = ResponseTypes.MESSAGE.value
-        self.assertEqual(response['type'], required)
+        required_type = ResponseTypes.MESSAGE
+        self.assertEqual(response['type'], required_type)
         self.assertEqual(response['message'], message)
-
--- test/test_core/test_game_entity.py
+++ test/test_core/test_game_entity.py
@@ -1,11 +1,8 @@
-import unittest
-from unittest.mock import patch
-from pyndemic.core.context import ContextRegistrationMeta, ContextError
+from unittest import TestCase
+from pyndemic.core.context import ContextError
 from pyndemic.core import api
-from queue import Queue
 
-
-from pyndemic.core.game_entity import GameEntity
+from pyndemic.core.game_entity import GameEntity, GameEntityCreationMeta
 
 
 class DumbQueue:
@@ -18,32 +15,76 @@ class DumbQueue:
     def get(self):
         return self.queue.pop(0)
 
+    def empty(self):
+        return not self.queue
 
-class GameEntityTestCase(unittest.TestCase):
+
+class GameEntityCreationMetaTestCase(TestCase):
+    def setUp(self):
+        class DumbEntity(metaclass=GameEntityCreationMeta):
+            pass
+
+        self._ctx = {'id': 'foo', 'content': 'Funny content!'}
+        self.test_class = DumbEntity
+
+    def test_call(self):
+        """Testing that every class created through GameEntityCreationMeta will
+        perform context search for the instance. Here we use TestCase instance
+        as a context holder and caller.
+        """
+        test_class = self.test_class
+        instance = test_class()
+        self.assertTrue(hasattr(instance, '_ctx'))
+        self.assertIs(instance._ctx, self._ctx)
+
+        del self._ctx
+        instance = test_class()
+        self.assertTrue(hasattr(instance, '_ctx'))
+        self.assertEqual(instance._ctx, {})
+
+
+class GameEntityTestCase(TestCase):
     def construct_controller_mock_class(self):
-        class MockController(metaclass=ContextRegistrationMeta,
-                             ctx_name='controller'):
+        """Help method for providing Controller-like class with "signals"
+        attribute.
+        """
+        class MockController:
             def __init__(self):
                 self.signals = DumbQueue()
-
-            def create_entity(self):
-                return GameEntity()
 
         return MockController
 
     def setUp(self):
         controller_class = self.construct_controller_mock_class()
         self.controller = controller_class()
+        self._ctx = {'id': 'mock_context', 'controller': self.controller}
 
     def test_assert_has_context(self):
+        entity = GameEntity()
+        entity.assert_has_context()
+
+        del self._ctx
         with self.assertRaises(ContextError):
-            self.entity = GameEntity()
-            self.entity.assert_has_context()
+            entity = GameEntity()
+            entity.assert_has_context()
 
     def test_emit_signal(self):
-        self.entity = self.controller.create_entity()
-        self.entity.signals_enabled = True
-        self.entity.emit_signal("message")
+        entity = GameEntity()
+
+        entity.signals_enabled = False
+        entity.emit_signal("message")
+        self.assertTrue(self.controller.signals.empty())
+
+        entity.signals_enabled = True
+        entity.emit_signal("message")
         received = self.controller.signals.get()
         required = api.message_response("message")
         self.assertEqual(required, received)
+
+    def test_emit_signal_without_context(self):
+        del self._ctx
+        entity = GameEntity()
+        entity.signals_enabled = True
+
+        with self.assertRaises(ContextError):
+            entity.emit_signal("message")
