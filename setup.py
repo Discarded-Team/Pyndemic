bd1e16d9fa1ab25bf3a0dc94774290ab25342524
D-Mantrid <geroyparagvay@gmail.com>
2020-07-08 18:24:30 +0300

    Made automatic version checking in `setup.py`.
    This adds more convenience for future changes of the project version number.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import re
+
 from setuptools import find_packages, setup
+
+
+with open('pyndemic.py') as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
     name='Pyndemic',
-    version='0.2.0',
+    version=version,
     author='Discarded Team',
     author_email='geroyparagvay@gmail.com',
     description='A future digital version of Pandemic board game',
