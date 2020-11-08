75ddd0c8d8da0575c9982c880c7fe83f518b1ebb
elgator <pavel.s.sokolov@gmail.com>
2020-11-08 18:26:41 +0300

    Refactor UI commands
    Move UI commands to separate methods.

--- pyndemic/ui/console.py
+++ pyndemic/ui/console.py
@@ -55,98 +55,16 @@ class ConsoleUI:
 
             if self.last_ui_command == 'chars' or \
                     self.last_ui_command == 'characters':
-                characters = response['game_data']['characters']
-                for ch in characters:
-                    output = "Player {}:\tin {}".format(ch['name'],
-                                                        ch['location'])
-                    self.io.send(output)
-                self.io.send("\n")
+                self._chars_ui_command(response)
 
-            if self.last_ui_command == 'hand':
-                ac_name = response['game_data']['active_character']
-                characters = response['game_data']['characters']
-                # getting active character subtree
-                ac_info = list(filter(lambda x: x['name'] == ac_name,
-                                      characters))[0]
-                hand = ac_info['hand']
+            elif self.last_ui_command == 'hand':
+                self._hand_ui_command(response)
 
-                self.io.send("\nPlayer {} hand:\n======================\n".format(
-                    ac_name
-                ))
-                for card in hand:
-                    #TODO this potentially will break for a non-city card
-                    output = "{}:\t{}".format(card['name'], card['colour'])
-                    self.io.send(output)
-                self.io.send("======================\n")
+            elif self.last_ui_command == 'labs':
+                self._labs_ui_command(response)
 
-            if self.last_ui_command == 'labs':
-                city_list = response['game_data']['cities']
-                labs = []
-                for city_name, city in city_list.items():
-                    if city['has_lab'] == True:
-                        # leave this as is if one would like to tweak the format
-                        labs.append("{}".format(city_name))
-
-                if len(labs) == 0:
-                    self.io.send("No labs has been built\n")
-                else:
-                    self.io.send("\nLabs are in:")
-                    self.io.send("\n".join(labs))
-                    self.io.send("\n") # produces two lines for some reason
-
-            if self.last_ui_command == 'infection':
-                infection_rate = response['game_data']['infection_rate']
-                epidemic_count = response['game_data']['epidemic_count']
-                outbreak_count = response['game_data']['outbreak_count']
-
-                diseases = response['game_data']['diseases']
-                disease_names = sorted((diseases.keys()))
-                city_dict = response['game_data']['cities']
-                infection_lvls = []
-                for city_name, city in city_dict.items():
-                    infection_lvls.append(city['infection_levels'].copy())
-                city_names = sorted((city_dict.keys()))
-
-                # total stats section
-                self.io.send("\nInfection status\n" + "=" * 40 + "\n")
-                self.io.send("Epidemic count {}".format(epidemic_count))
-                self.io.send("Disease spreads {} cities each time".
-                             format(infection_rate))
-                self.io.send("{} outbreaks so far".format(outbreak_count))
-                self.io.send("=" * 40 + "\n")
-
-                header_output = "\t\t\t" + "\t".join(disease_names) + "\n"
-                self.io.send(header_output)
-                self.io.send("-" * 40 + "\n")
-
-                # disease status section
-                infect_lvl_output = "Health\t\t"
-                cured_output = "Cured?\t\t"
-                for d in disease_names:
-                    infect_lvl_output += "\t{}".format(
-                        diseases[d]['public_health'])
-                    cured_output += "\t{}".format(
-                        diseases[d]['cured'])
-                infect_lvl_output += "\n"
-                cured_output += "\n"
-                self.io.send(infect_lvl_output)
-                self.io.send(cured_output)
-                self.io.send("-" * 40 + "\n")
-
-                # city infection status section
-                for c in city_names:
-                    city_output = c
-                    if len(c) < 8:
-                        city_output += "\t"
-                    city_output += "\t"
-                    for d in disease_names:
-                        lvl_output = city_dict[c]['infection_levels'][d]
-                        if lvl_output == 0:
-                            lvl_output = "."
-                        city_output += "\t{}".format(lvl_output)
-                    self.io.send(city_output)
-
-                self.io.send("=" * 16 + "\n")
+            elif self.last_ui_command == 'infection':
+                self._infection_ui_command(response)
 
             self.last_ui_command = None
             self.last_ui_command_args = None
@@ -157,6 +75,100 @@ class ConsoleUI:
         if 'message' in response:
             io_response = response['message']
             self.io.send(io_response)
+
+    def _chars_ui_command(self, response):
+        characters = response['game_data']['characters']
+        for ch in characters:
+            output = "Player {}:\tin {}".format(ch['name'],
+                                                ch['location'])
+            self.io.send(output)
+        self.io.send("\n")
+
+    def _hand_ui_command(self, response):
+        ac_name = response['game_data']['active_character']
+        characters = response['game_data']['characters']
+        # getting active character subtree
+        ac_info = list(filter(lambda x: x['name'] == ac_name,
+                              characters))[0]
+        hand = ac_info['hand']
+
+        self.io.send("\nPlayer {} hand:\n======================\n".format(
+            ac_name
+        ))
+        for card in hand:
+            # TODO this potentially will break for a non-city card
+            output = "{}:\t{}".format(card['name'], card['colour'])
+            self.io.send(output)
+        self.io.send("======================\n")
+
+    def _labs_ui_command(self, response):
+        city_list = response['game_data']['cities']
+        labs = []
+        for city_name, city in city_list.items():
+            if city['has_lab'] == True:
+                # leave this as is if one would like to tweak the format
+                labs.append("{}".format(city_name))
+
+        if len(labs) == 0:
+            self.io.send("No labs has been built\n")
+        else:
+            self.io.send("\nLabs are in:")
+            self.io.send("\n".join(labs))
+            self.io.send("\n")  # produces two lines for some reason
+
+    def _infection_ui_command(self, response):
+        infection_rate = response['game_data']['infection_rate']
+        epidemic_count = response['game_data']['epidemic_count']
+        outbreak_count = response['game_data']['outbreak_count']
+
+        diseases = response['game_data']['diseases']
+        disease_names = sorted((diseases.keys()))
+        city_dict = response['game_data']['cities']
+        city_names = sorted((city_dict.keys()))
+
+        break_length = 56
+        # total stats section
+        self.io.send("\nInfection status\n" + "=" * break_length)
+        self.io.send("Epidemic count {}".format(epidemic_count))
+        self.io.send("Disease spreads {} cities each time".
+                     format(infection_rate))
+        self.io.send("{} outbreaks so far".format(outbreak_count))
+        self.io.send("=" * break_length)
+
+        header_output = "\t\t\t" + "\t".join(disease_names)
+        self.io.send(header_output)
+        self.io.send("-" * break_length)
+
+        # disease status section
+        infect_lvl_output = "Public Health\t"
+        cured_output = "Cured?\t\t"
+        for d in disease_names:
+            infect_lvl_output += "\t{}".format(
+                diseases[d]['public_health'])
+            cured_output += "\t{}".format(
+                diseases[d]['cured'])
+        self.io.send(infect_lvl_output)
+        self.io.send(cured_output)
+        self.io.send("-" * break_length)
+
+        # city infection status section
+        for c in city_names:
+            city_output = c
+            total_disease = 0
+            if len(c) < 8: # fix for names that are too short
+                city_output += "\t"
+            city_output += "\t"
+            for d in disease_names:
+                lvl_output = city_dict[c]['infection_levels'][d]
+                total_disease += lvl_output
+                # better visibility for other numbers in a table
+                if lvl_output == 0:
+                    lvl_output = "."
+                city_output += "\t{}".format(lvl_output)
+            if total_disease > 0:
+                self.io.send(city_output)
+
+        self.io.send("=" * break_length + "\n")
 
     def parse_user_input(self, input_request):
         input_request = input_request.split()
